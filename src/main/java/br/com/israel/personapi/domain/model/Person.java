a5198f1817cd50da8fa9920a2afaf9c7fa961db2
israelnp <israelpereira1984@gmail.com>
2021-07-14 17:26:22 -0300

    Add Person model

---
+++ src/main/java/br/com/israel/personapi/domain/model/Person.java
@@ -0,0 +1,24 @@
+package br.com.israel.personapi.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String cpf;
+    private LocalDate birthDate;
+    private List<Phone> phones = new ArrayList<>();
+}
