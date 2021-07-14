e7e2d84eda8ec487ad3f06b8b659e29e8a172ad1
israelnp <israelpereira1984@gmail.com>
2021-07-14 17:21:23 -0300

    Add enum PhoneType

---
+++ src/main/java/br/com/israel/personapi/domain/model/PhoneType.java
@@ -0,0 +1,15 @@
+package br.com.israel.personapi.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+    HOME("Home"),
+    MOBILE("Mobile"),
+    COMMERCIAL("Commercial");
+
+    private final String description;
+}
