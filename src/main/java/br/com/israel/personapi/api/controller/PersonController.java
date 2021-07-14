7c63b77e7940f6afbb29a62cc7475e268b9b32e7
israelnp <israelpereira1984@gmail.com>
2021-07-14 18:07:50 -0300

    add PersonController

---
+++ src/main/java/br/com/israel/personapi/api/controller/PersonController.java
@@ -0,0 +1,12 @@
+package br.com.israel.personapi.api.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/people", produces = MediaType.APPLICATION_JSON_VALUE)
+public class PersonController {
+
+
+}
