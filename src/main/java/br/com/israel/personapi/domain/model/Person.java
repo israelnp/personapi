157388cb1697ca68bf6b3e993cfaf047449cee1a
israelnp <israelpereira1984@gmail.com>
2021-07-14 17:38:38 -0300

    Map Person model

--- src/main/java/br/com/israel/personapi/domain/model/Person.java
+++ src/main/java/br/com/israel/personapi/domain/model/Person.java
@@ -5,20 +5,28 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String firstName;
+    @Column(nullable = false)
     private String lastName;
+    @Column(nullable = false, unique = true)
     private String cpf;
     private LocalDate birthDate;
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.ALL})
     private List<Phone> phones = new ArrayList<>();
 }
